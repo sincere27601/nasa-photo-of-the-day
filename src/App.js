afda542563d4875f90bed661b8a91f0727c306fc
Mike Raysor <michaelraysor1@gmail.com>
2020-02-04 11:55:12 -0500

    updated App.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
+
+const axios = required('axios');
+
+function getNasaData() {
+  axios.get('http://api.github.com/users/sincere27601')
+  .then(res => {
+    console.log(err);
+  });
+}
+
+function User({ id }) {
+  const [user, setUser] = useState("");
+  const fetchUser = (id) =>
+  fetch(``)
+}
 
 function App() {
   return (

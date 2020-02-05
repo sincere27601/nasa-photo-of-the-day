7fbe6aa1168e47a68fceef06ac584f339f3b296c
Mike Raysor <michaelraysor1@gmail.com>
2020-02-05 12:13:56 -0500

    updated key in API

--- src/Api.js
+++ src/Api.js
@@ -5,7 +5,7 @@ import " ./styles.css";
 
 function user({ id }) {
     const [user, setUser] = useState("Guest");
-    const fetchUser = (id) => fetch(`https://api.nasa.gov/planetary/apod?api_key=dffHub9O9B7a8PkNwlIGicZTqwQ5NfUcP3QSXtof`)
+    const fetchUser = (id) => fetch(`https://api.nasa.gov/#apod?api_key=dffHub9O9B7a8PkNwlIGicZTqwQ5NfUcP3QSXtof`)
     .then (response => response.json())
     .then (user => {
         console.log(user);

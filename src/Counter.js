ee05eabc70689bc8b291b3181ba4aa7b5b01cae2
Mike Raysor <michaelraysor1@gmail.com>
2020-02-05 19:36:04 -0500

    updaed Counter.js

---
+++ src/Counter.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+function counter ({setCount}) {
+    return <h2>The count is {2}!</h2>;
+}
+function App() {
+    const[isCounterVisible, setCounterVisible] = useState(false);
+    const[count, setCount] = useState(0);
+
+    return (
+        <div className="app">
+            <button onClick={e => setIsCounterVisible(true)}>Mount counter</button>
+            <button onClick={e => setIsCounterVisible(false)}>Destroy counter</button>
+            <button onClick={ e => setCount(count + 1)}>Increase count</button>
+            </div>
+    );
+}
+    const [count, setCount] = useState(initialCount);
+    return <h2></h2>
+        
+ 
---
+++ src/DataProps.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import " ./styles.css";
+
+function App(props){
+    const friends = props.friends;
+    return <div> Hello, { friends[0].name}!How are you?</div>;
+
+}
+
+const rootElemennt = document.getElementById("root");
+ReactDOM.render(<app friends = {siths} />, rootElement);

f8e66a0e149b74efadc1c1f5793b6f0d2abb4811
Mike Raysor <michaelraysor1@gmail.com>
2020-02-07 12:14:50 -0500

    updated Style for project int he index.js section

--- src/DataProps.js
+++ src/DataProps.js
@@ -9,5 +9,7 @@ function App(props){
 
 }
 
+
+
 const rootElemennt = document.getElementById("root");
 ReactDOM.render(<app friends = {siths} />, rootElement);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect} from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./styles.css";
 import App from "./App";
+import StarCard from " ./components/StarCard";
 
+//Styled Components
 
-
+const StarCard = styled.div`
+background: #99f3eb;
+color: black;
+width: 200px;
+max - height: 300px;
+display: flex;
+flex - direction: column;
+align - items: center;
+justify - content: space - between;
+margin - bottom: 20px;
+`;
 
